f11ec0dfaebf3c151c938ad5e53931db4f277f66
Max <maximummana.y@gmail.com>
2021-12-10 17:48:52 +0300

    добавил простую навигацию, анимацию, новый экран

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/ui/pages/details_page.dart';
 import 'package:untitled/ui/pages/home_page.dart';
 
 void main() => runApp(const AnyWayApp());
@@ -8,10 +9,16 @@ class AnyWayApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Theme.of(context).
     return MaterialApp(
+      theme: ThemeData.light().copyWith(
+
+      ),
+
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => const HomePage(),
+        '/details': (context) => const DetailsPage(),
       },
     );
   }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/ui/widgets/city_card_widget.dart';
 
@@ -11,12 +10,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late final TextEditingController _textController;
-  var _currentIndex = 1;
 
   @override
   void initState() {
-    _textController = TextEditingController();
     super.initState();
+    _textController = TextEditingController();
   }
 
   @override
@@ -26,6 +24,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   final GlobalKey<ScaffoldState> _homeKey = GlobalKey();
+  final _appBarKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -33,124 +32,146 @@ class _HomePageState extends State<HomePage> {
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
         key: _homeKey,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 2.0,
-          shadowColor: Colors.black38,
-          title: SizedBox(
-            height: 40.0,
-            child: TextField(
-              controller: _textController,
-              scrollPhysics: const BouncingScrollPhysics(),
-              cursorColor: Colors.deepOrange,
-              style: const TextStyle(color: Colors.black, fontSize: 18.0),
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.search, color: Colors.black54),
-                suffixIcon: IconButton(
-                  splashRadius: 16.0,
-                  icon: const Icon(Icons.clear, color: Colors.black54),
-                  onPressed: () => _textController.text = "",
+        body: NestedScrollView(
+          physics: const BouncingScrollPhysics(),
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                shadowColor: Colors.transparent,
+                title: SizedBox(
+                  height: 40.0,
+                  child: TextField(
+                    controller: _textController,
+                    scrollPhysics: const BouncingScrollPhysics(),
+                    cursorColor: Colors.deepOrange,
+                    style: const TextStyle(color: Colors.black, fontSize: 18.0),
+                    decoration: InputDecoration(
+                      prefixIcon:
+                          const Icon(Icons.search, color: Colors.black54),
+                      suffixIcon: IconButton(
+                        splashRadius: 16.0,
+                        icon: const Icon(Icons.clear, color: Colors.black54),
+                        onPressed: () => _textController.text = "",
+                      ),
+                      contentPadding: const EdgeInsets.fromLTRB(16, 4, 4, 16),
+                      hintText: 'Search your journey...',
+                      hintStyle: const TextStyle(
+                          color: Colors.black38, fontSize: 18.0),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.grey, width: 2.0),
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Colors.deepOrange, width: 2.0),
+                        borderRadius: BorderRadius.circular(32.0),
+                      ),
+                    ),
+                  ),
                 ),
-                contentPadding: const EdgeInsets.fromLTRB(16, 4, 4, 16),
-                hintText: 'Search your journey...',
-                hintStyle:
-                const TextStyle(color: Colors.black38, fontSize: 18.0),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.grey, width: 2.0),
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                  const BorderSide(color: Colors.deepOrange, width: 2.0),
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
-              ),
-            ),
-          ),
-          actions: [
-            IconButton(
-              splashRadius: 24.0,
-              iconSize: 36.0,
-              splashColor: Colors.deepOrange.withOpacity(0.3),
-              icon: const Icon(
-                Icons.sort,
-                color: Colors.black54,
-              ),
-              onPressed: () {
-                showModalBottomSheet<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Container(
-                        color: Colors.white,
-                        height: 300,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Text('Modal BottomSheet'),
-                              ElevatedButton(
-                                child: const Text('Close BottomSheet'),
-                                onPressed: () => Navigator.pop(context),
-                              )
-                            ],
+                actions: [
+                  IconButton(
+                    splashRadius: 24.0,
+                    iconSize: 36.0,
+                    splashColor: Colors.deepOrange.withOpacity(0.3),
+                    icon: const Icon(
+                      Icons.sort,
+                      color: Colors.black54,
+                    ),
+                    onPressed: () {
+                      showModalBottomSheet<void>(
+                          // barrierColor: Colors.bla.withOpacity(0.1),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(32.0)),
                           ),
-                        ),
-                      );
-                    });
-              },
-            )
-          ],
+                          context: context,
+                          builder: (BuildContext context) {
+                            return SizedBox(
+                              height: 300,
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    const SizedBox(height: 8.0),
+                                    Container(
+                                      width: 64.0,
+                                      height: 8.0,
+                                      decoration: const BoxDecoration(
+                                        color: Color(0x7736382E),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(16.0)),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8.0),
+                                    const Text('Modal BottomSheet'),
+                                    const SizedBox(height: 8.0),
+                                    ElevatedButton(
+                                      child: const Text('Close BottomSheet'),
+                                      onPressed: () => Navigator.pop(context),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                  )
+                ],
+                forceElevated: innerBoxIsScrolled,
+              ),
+            ];
+          },
+          body: GridView.builder(
+              padding: const EdgeInsets.all(8.0),
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+                childAspectRatio: 0.75,
+              ),
+              itemCount: 32,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  child: CityCard(id: index),
+                  onTap: () => Navigator.of(context)
+                      .pushNamed("/details", arguments: {'id': index}),
+                );
+              }),
         ),
-        body: Scrollbar(
-          radius: const Radius.circular(8),
-          child: Padding(
-            padding: const EdgeInsets.only(right: 4.0),
-            child: GridView.builder(
-                padding: const EdgeInsets.all(8.0),
-                physics: const BouncingScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16.0,
-                  mainAxisSpacing: 16.0,
-                  childAspectRatio: 0.75,
-                ),
-                itemCount: 32,
-                itemBuilder: (BuildContext context, int index) {
-                  return CityCard();
-                }),
-          ),
-        ),
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _currentIndex,
-          itemCornerRadius: 24,
-          curve: Curves.easeIn,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          onItemSelected: (index) => setState(() => _currentIndex = index),
-          items: <BottomNavyBarItem>[
-            BottomNavyBarItem(
-              icon: const Icon(Icons.home),
-              title: const Text('Home'),
-              activeColor: Colors.deepOrangeAccent,
-              inactiveColor: Colors.black54,
-              textAlign: TextAlign.center,
-            ),
-            BottomNavyBarItem(
-              icon: const Icon(Icons.luggage),
-              title: const Text('Journeys'),
-              activeColor: Colors.deepOrangeAccent,
-              inactiveColor: Colors.black54,
-              textAlign: TextAlign.center,
-            ),
-            BottomNavyBarItem(
-              icon: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              activeColor: Colors.deepOrangeAccent,
-              inactiveColor: Colors.black54,
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
+        // bottomNavigationBar: BottomNavyBar(
+        //   selectedIndex: _currentIndex,
+        //   itemCornerRadius: 24,
+        //   curve: Curves.easeIn,
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //   onItemSelected: (index) => setState(() => _currentIndex = index),
+        //   items: <BottomNavyBarItem>[
+        //     BottomNavyBarItem(
+        //       icon: const Icon(Icons.home),
+        //       title: const Text('Home'),
+        //       activeColor: Colors.deepOrangeAccent,
+        //       inactiveColor: Colors.black54,
+        //       textAlign: TextAlign.center,
+        //     ),
+        //     BottomNavyBarItem(
+        //       icon: const Icon(Icons.luggage),
+        //       title: const Text('Journeys'),
+        //       activeColor: Colors.deepOrangeAccent,
+        //       inactiveColor: Colors.black54,
+        //       textAlign: TextAlign.center,
+        //     ),
+        //     BottomNavyBarItem(
+        //       icon: const Icon(Icons.settings),
+        //       title: const Text('Settings'),
+        //       activeColor: Colors.deepOrangeAccent,
+        //       inactiveColor: Colors.black54,
+        //       textAlign: TextAlign.center,
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
