b55dcd2a54c6c209fae2fd03398bd423b5ad1687
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2021-12-13 12:00:37 +0300

    Fixed merged version + icons

--- lib/ui/widgets/city_card_widget.dart
+++ lib/ui/widgets/city_card_widget.dart
@@ -23,88 +23,95 @@ class _CityCardState extends State<CityCard> {
 
   @override
   Widget build(BuildContext context) => Hero(
-      tag: widget.city.id,
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(16.0)),
-          boxShadow: [BoxShadow(color: Colors.black, blurRadius: 2.0)],
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: CachedNetworkImage(
-                  fit: BoxFit.fitHeight,
-                  imageUrl: widget.city.imgSrc,
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 90,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                    color: Colors.white,
+        tag: widget.city.id,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(16.0)),
+            boxShadow: [BoxShadow(color: Colors.black, blurRadius: 2.0)],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: CachedNetworkImage(
+                    fit: BoxFit.fitHeight,
+                    imageUrl: widget.city.imgSrc,
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: Text(
-                            widget.city.name,
-                            style: const TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              overflow: TextOverflow.ellipsis,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 90,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Text(
+                              widget.city.name,
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: const [
-                            Icon(Icons.ac_unit_rounded,
-                                color: Colors.lightBlueAccent, size: 32.0),
-                            Icon(Icons.account_balance_rounded,
-                                color: Colors.grey, size: 32.0),
-                            Icon(Icons.air_rounded,
-                                color: Colors.cyanAccent, size: 32.0),
-                            Icon(Icons.eco,
-                                color: Colors.lightGreenAccent, size: 32.0),
-                          ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FittedBox(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                if (widget.city.filter.price == 2)
+                                  const Icon(
+                                      Icons.ac_unit_rounded,
+                                      color: Colors.lightBlueAccent,
+                                      size: 32.0,
+                                  ),
+                                Icon(Icons.account_balance_rounded,
+                                    color: Colors.grey, size: 32.0),
+                                Icon(Icons.air_rounded,
+                                    color: Colors.cyanAccent, size: 32.0),
+
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Material(
-                  color: Colors.transparent,
-                  child: IconButton(
-                    onPressed: () => _addToFavorite(widget.city),
-                    icon: widget.isFavorite
-                        ? const Icon(Icons.favorite, color: Colors.red)
-                        : const Icon(Icons.favorite_border, color: Colors.grey),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: IconButton(
+                      onPressed: () => _addToFavorite(widget.city),
+                      icon: widget.isFavorite
+                          ? const Icon(Icons.favorite, color: Colors.red)
+                          : const Icon(Icons.favorite_border,
+                              color: Colors.grey),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
 
   void _addToFavorite(City item) {
     if (widget.isFavorite) {
@@ -138,45 +145,46 @@ class BottomDialog {
       isScrollControlled: true,
       context: context,
       builder: (context) => Wrap(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-                borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(32),
-                  topLeft: Radius.circular(32),
-                ),
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).backgroundColor,
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(32),
+                topLeft: Radius.circular(32),
               ),
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Container(
-                        height: 8,
-                        width: 56,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                          color: Theme.of(context).primaryColorLight,
-                        ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 8,
+                      width: 56,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(16.0)),
+                        color: Theme.of(context).primaryColorLight,
                       ),
                     ),
-                    const SizedBox(height: 16),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: DefaultTextStyle(
-                        style: Theme.of(context).textTheme.headline1!,
-                        child: title ?? Spacer(),
-                      ),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: DefaultTextStyle(
+                      style: Theme.of(context).textTheme.headline1!,
+                      child: title ?? Spacer(),
                     ),
-                    Center(child: body),
-                  ],
-                ),
+                  ),
+                  Center(child: body),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
