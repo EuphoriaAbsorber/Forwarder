e21998523aeff381134d6e4d4c80070dfcca4b71
Maxim <maximummana.y@gmail.com>
2021-12-17 12:27:45 +0300

    changes: добавил иконку и поменял имя в android проекте.

--- test/ui_test.dart
+++ test/ui_test.dart
@@ -1,9 +1,7 @@
-import 'package:anyway/city_list/presentation/search_widget.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Search Widget testing', (WidgetTester tester) async {
-
+  testWidgets('Widget testing', (tester) async {
+    
   });
 }
