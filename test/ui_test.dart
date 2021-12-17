e83f6159037dfc39878aa7b0cda0a5999eaaee20
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2021-12-17 13:11:49 +0300

    widget test

--- test/ui_test.dart
+++ test/ui_test.dart
@@ -1,9 +1,37 @@
-import 'package:anyway/city_list/presentation/search_widget.dart';
+import 'package:anyway/city_list/presentation/widgets/filter_choice_chip.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Search Widget testing', (WidgetTester tester) async {
 
+  Widget createWidgetForTesting({required Widget child})=> MaterialApp(
+      home: child,
+    );
+  
+  testWidgets('Filter choice chip Widget', (WidgetTester tester) async {
+
+    const testKey = Key('testKey');
+
+    await tester.pumpWidget(createWidgetForTesting(
+      child: Scaffold(
+        body: FilterChoiceChip(
+          selected: false,
+          onSelected: (bool selected) => selected = !selected,
+          category: "дёшево",
+          icon: Icon(
+            Icons.paid,
+            color: Colors.yellow,
+          ),
+          key: testKey,
+        ),
+      ),
+    )
+    );
+
+    final keyFinder = find.byKey(testKey);
+    final textFinder = find.text("дёшево");
+
+    expect(textFinder, findsOneWidget);
+    expect(keyFinder, findsOneWidget);
   });
 }
